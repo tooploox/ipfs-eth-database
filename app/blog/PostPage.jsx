459ef4f61aea01e9003a7c3a3c676604404ab2fe
Michal Zalecki <michal@michalzalecki.com>
2018-06-08 10:08:46 +0200

    Init app

---
+++ app/blog/PostPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function PostPage() {
+  return (
+    <div>
+      <h1>PostPage</h1>
+    </div>
+  );
+}
---
+++ app/home/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function HomePage() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
+}
