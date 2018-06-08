03ca8a4d35d6fe65b143cb23b6e56d7eeef42347
Michal Zalecki <michal@michalzalecki.com>
2018-06-08 10:31:39 +0200

    Configure routing

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+
 import { HomePage } from "./home/HomePage";
+import { ArchivePage } from "./blog/ArchivePage";
+import { PostPage } from "./blog/PostPage";
 
 function App() {
   return (
-    <HomePage />
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/u/:address" component={ArchivePage} />
+        <Route exact path="/u/:address/:hash" component={PostPage} />
+        <Redirect to="/" />
+      </Switch>
+    </HashRouter>
   );
 }
 
